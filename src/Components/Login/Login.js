93e67fbf4b0cc1f16adf7b7806f32147a11c42f7
Architgarg603 <architgarg603@gmail.com>
2022-07-03 02:04:31 +0530

    login

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from './Login.module.scss'
+import logo from '../../Assets/logo.JPG'
+import { Link } from 'react-router-dom'
+function Login() {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.logo}>
+                <img src={logo} />
+            </div>
+            <div className={style.details}>
+                <input type='text' placeholder='Email' />
+                <input type='password' placeholder='Password' />
+                <div className={style.btn}>Login</div>
+                <Link to='/register     '>
+                    <div className={style.nav}>Sign Up</div>
+                </Link>
+            </div>
+
+            <div className={style.bottom}>Go Air</div>
+
+        </div>
+    )
+}
+
+export default Login
